f6914cf71ab17f911427cc7ed758ba6e2eb3a757
robinsg <glenn.robinson@rsipvm.com>
2018-12-13 08:27:40 +0000

    Add get_ltmstats.py to show the current settings

--- get_ltmprefs.py
+++ get_ltmprefs.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
 # Version 1
-# Set Longterm and Aggregate PCM stats on or off
+# Get and show Longterm and Aggregate PCM stats settings
 
 import hmc_pcm as hmc
-import time
-import os
 import sys
+import os
 
-if len(sys.argv) != 6:   # six including the program name entry [0]
-    print("Usage: %s HMC-hostname HMC-username HMC-password true|false servername|ALL" %(sys.argv[0]))
-    sys.exit(1)
+if len(sys.argv) != 5:   # five including the program name entry [0]
+	print("Usage: %s HMC-hostname HMC-username HMC-password  servername|ALL" %(sys.argv[0]))
+	sys.exit(1)
 hostname=sys.argv[1]
 user    =sys.argv[2]
 password=sys.argv[3]
-toggle  =sys.argv[4]
-mgServer=sys.argv[5]
+mgServer=sys.argv[4]
 
-## Make sure the toggle is true or false
-if not (toggle == "true" or toggle == "false"):
-    print("%s: Toggle parameter must be true or false" %(sys.argv[0]))
-    sys.exit(1) 
+print("HMC hostanme=%s User=%s Password=%s Server(s)=%s"  %( hostname, user, password, mgServer))
 
-print("HMC hostanme=%s User=%s Password=%s On/Off=%s Server(s)=%s"  %( hostname, user, password, toggle, mgServer))
-
-debug=True
+debug=False
 debugDir = "./debug"
 
 if not os.path.exists(debugDir):
-        os.makedirs(debugDir)
+	os.makedirs(debugDir)
 
 print("-> Logging on to %s as user %s" % (hostname,user))
 hmc = hmc.HMC(hostname, user, password)
@@ -38,8 +31,8 @@ prefstripped = hmc.get_stripped_preferences_pcm()
 
 ## If debug is enabled save a copy of the current prefernces
 if debug:
-    print("-> Saving current repferences to directory: %s" %debugDir)
-    hmc.save_to_file("server_perferences.xml",prefstripped)
+	print("-> Saving current repferences to directory: %s" %debugDir)
+	hmc.save_to_file("server_perferences.xml",prefstripped)
 
 print("-> Parse current Preferences")
 serverlist = hmc.parse_prefs_pcm(prefstripped)  # returns a list of dictionaries one per Server
@@ -47,21 +40,15 @@ perflist = []
 all_true = True
 
 for num,server in enumerate(serverlist):
-    ## Process all managed servers for this HMC
-    if mgServer == "ALL":
-        print("-> Processing server=%-16s"%server['name'])
-        print("-> Set LTM and AGG preferences to: %-5s" %toggle)
-        hmc.set_ltm_flag(server['name'], toggle)
-        hmc.set_preferences_pcm()
-    ## Process just the individual managed server specified in parameter 5    
-    elif not mgServer == "ALL":
-        if server['name'] == mgServer:
-            print("-> Processing server=%-16s"%server['name'])
-            print("-> Set LTM and AGG preferences to: %-5s" %toggle)
-            hmc.set_ltm_flag(server['name'], toggle)
-            hmc.set_preferences_pcm()
-        else:
-            print("-> Ignoring server=%-16s"%server['name'])
+	## Process all managed servers for this HMC
+	if mgServer == "ALL":
+		print('-> Server name=%-16s agg=%-5s longterm=%-5s' %(server['name'], server['agg'], server['lterm']))
+	## Process just the individual managed server specified in parameter 5    
+	elif not mgServer == "ALL":
+		if server['name'] == mgServer:
+			print('-> Server name=%-16s agg=%-5s longterm=%-5s' %(server['name'], server['agg'], server['lterm']))
+	else:
+		print("-> Ignoring server=%-16s"%server['name'])
 
 print("Logging off the HMC")
 hmc.logoff()
